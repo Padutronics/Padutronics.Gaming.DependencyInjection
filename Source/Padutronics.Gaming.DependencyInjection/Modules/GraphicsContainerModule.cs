d67a5cc5f848a68bb97be0da4e31b30ac43030b1
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-28 12:22:09 +0200

    Register type Padutronics.Gaming.Graphics.Resources.ResourceFactory

--- Source/Padutronics.Gaming.DependencyInjection/Modules/GraphicsContainerModule.cs
+++ Source/Padutronics.Gaming.DependencyInjection/Modules/GraphicsContainerModule.cs
@@ -1,5 +1,6 @@
 using Padutronics.DependencyInjection;
 using Padutronics.Gaming.Graphics;
+using Padutronics.Gaming.Graphics.Resources;
 
 namespace Padutronics.Gaming.DependencyInjection.Modules;
 
@@ -8,5 +9,7 @@ internal sealed class GraphicsContainerModule : IContainerModule
     public void Load(IContainerBuilder containerBuilder)
     {
         containerBuilder.For<RenderViewOptions>().UseSelf().InstancePerDependency();
+
+        containerBuilder.For<IResourceFactory>().Use<ResourceFactory>().InstancePerDependency();
     }
 }
