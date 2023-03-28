a733e05eeb67ce7ef52e9e56f6aa651b7c948848
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-28 12:15:52 +0200

    Register types Padutronics.Gaming.Components<Padutronics.Gaming.Components.Behaviors.IAnimationBehavior>, Padutronics.Gaming.Components.Factories.AnimationComponentFactory, Padutronics.Gaming.Components.Behaviors.Adapters.AnimationBehaviorToComponentAdapter

--- Source/Padutronics.Gaming.DependencyInjection/Modules/ComponentsContainerModule.cs
+++ Source/Padutronics.Gaming.DependencyInjection/Modules/ComponentsContainerModule.cs
@@ -1,5 +1,8 @@
 using Padutronics.DependencyInjection;
+using Padutronics.Gaming.Components;
+using Padutronics.Gaming.Components.Behaviors;
 using Padutronics.Gaming.Components.Behaviors.Adapters;
+using Padutronics.Gaming.Components.Factories;
 
 namespace Padutronics.Gaming.DependencyInjection.Modules;
 
@@ -8,5 +11,17 @@ internal sealed class ComponentsContainerModule : IContainerModule
     public void Load(IContainerBuilder containerBuilder)
     {
         containerBuilder.For<IBehaviorAdapterFactory>().UseFactory();
+
+        RegisterBehavior<IAnimationBehavior, AnimationComponentFactory, AnimationBehaviorToComponentAdapter>(containerBuilder);
+    }
+
+    private void RegisterBehavior<TBehavior, TFactory, TAdapter>(IContainerBuilder containerBuilder)
+        where TBehavior : IBehavior
+        where TFactory : class, IComponentFactory<TBehavior>
+        where TAdapter : BehaviorToComponentAdapter<TBehavior>
+    {
+        containerBuilder.For<IComponentSource>().Use<ComponentSource<TBehavior>>().SingleInstance();
+        containerBuilder.For<IComponentFactory<TBehavior>>().Use<TFactory>().InstancePerDependency();
+        containerBuilder.For<TAdapter>().UseSelf().InstancePerDependency();
     }
 }
