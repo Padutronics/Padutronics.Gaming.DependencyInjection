44f2d67df18fc317df45017e3c950f74137ed04f
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-28 12:25:58 +0200

    Register type Padutronics.Gaming.Inputs.InputDevice<Padutronics.Gaming.Inputs.Keyboards.MouseState>

--- Source/Padutronics.Gaming.DependencyInjection/Modules/InputsContainerModule.cs
+++ Source/Padutronics.Gaming.DependencyInjection/Modules/InputsContainerModule.cs
@@ -2,6 +2,7 @@ using Padutronics.DependencyInjection;
 using Padutronics.Gaming.Frames;
 using Padutronics.Gaming.Inputs;
 using Padutronics.Gaming.Inputs.Keyboards;
+using Padutronics.Gaming.Inputs.Mouses;
 
 namespace Padutronics.Gaming.DependencyInjection.Modules;
 
@@ -17,5 +18,7 @@ internal sealed class InputsContainerModule : IContainerModule
 
         containerBuilder.For<IInputDevice>().Use<InputDevice<KeyboardState>>().SingleInstance();
         containerBuilder.For<IKeyboard, IKeyboardManager, IInputDeviceManager<KeyboardState>>().Use<Keyboard>().SingleInstance();
+
+        containerBuilder.For<IInputDevice>().Use<InputDevice<MouseState>>().SingleInstance();
     }
 }
