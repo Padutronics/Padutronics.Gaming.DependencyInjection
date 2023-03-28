87dd140250474599adaf8702e2d6edf34ec077b1
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-28 12:23:45 +0200

    Register type Padutronics.Gaming.Inputs.Input

--- Source/Padutronics.Gaming.DependencyInjection/Modules/GamingContainerModule.cs
+++ Source/Padutronics.Gaming.DependencyInjection/Modules/GamingContainerModule.cs
@@ -17,6 +17,7 @@ public sealed class GamingContainerModule : IContainerModule
             .IncludeModule<BootstrappingContainerModule>()
             .IncludeModule<ComponentsContainerModule>()
             .IncludeModule<FramesContainerModule>()
-            .IncludeModule<GraphicsContainerModule>();
+            .IncludeModule<GraphicsContainerModule>()
+            .IncludeModule<InputsContainerModule>();
     }
 }
---
+++ Source/Padutronics.Gaming.DependencyInjection/Modules/InputsContainerModule.cs
@@ -0,0 +1,12 @@
+using Padutronics.DependencyInjection;
+using Padutronics.Gaming.Inputs;
+
+namespace Padutronics.Gaming.DependencyInjection.Modules;
+
+internal sealed class InputsContainerModule : IContainerModule
+{
+    public void Load(IContainerBuilder containerBuilder)
+    {
+        containerBuilder.For<Input>().UseSelf().InstancePerDependency();
+    }
+}
