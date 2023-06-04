e2b4e16804f566cb09b0e45c522dc4f638cc6d35
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-06-04 20:34:48 +0200

    Register type Padutronics.Gaming.Diagnostics.Console.Behaviors.ConsoleInputBehavior

--- Source/Padutronics.Gaming.DependencyInjection/Modules/DiagnosticsContainerModule.cs
+++ Source/Padutronics.Gaming.DependencyInjection/Modules/DiagnosticsContainerModule.cs
@@ -1,5 +1,7 @@
 using Padutronics.DependencyInjection;
+using Padutronics.Gaming.Components.Behaviors;
 using Padutronics.Gaming.Diagnostics.Console;
+using Padutronics.Gaming.Diagnostics.Console.Behaviors;
 
 namespace Padutronics.Gaming.DependencyInjection.Modules;
 
@@ -9,5 +11,7 @@ internal sealed class DiagnosticsContainerModule : IContainerModule
     {
         containerBuilder.For<ConsoleOptions>().UseSelf().SingleInstance();
         containerBuilder.For<IConsoleVisibilityManager, IConsoleVisibilityProvider>().Use<ConsoleVisibilityManager>().SingleInstance();
+
+        containerBuilder.For<IInputBehavior>().Use<ConsoleInputBehavior>().SingleInstance();
     }
 }
